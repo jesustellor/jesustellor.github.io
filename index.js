359d68f577682ca94fab082fc59c4e679ca2660c
Jesus T. <jesustellor@gmail.com>
2024-10-27 07:26:17 +0000

    fixed gaps in mobile view and changed moon imge

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import * as THREE from 'three';
   const loader = new THREE.TextureLoader();
   const cubeTexture = loader.load('https://avatars.githubusercontent.com/u/22569672?s=400&u=4959806090e507fbb811869e5b8785eb015f6e66&v=4');
   cubeTexture.colorSpace = THREE.SRGBColorSpace;
-  const circleTexture = loader.load('/images/matrixImage.png');
+  const circleTexture = loader.load('/images/moon.jpg');
   circleTexture.colorSpace = THREE.SRGBColorSpace;
   const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true, canvas: mypic});
   const renderer2 = new THREE.WebGLRenderer({alpha: true, antialias: true, canvas: matrix});
@@ -15,14 +15,14 @@ import * as THREE from 'three';
   const near = 0.1;
   const far = 5;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 2;
+  camera.position.z = 1.5;
 
   const fov2 = 75;
   const aspect2 = window.innerWidth / window.innerHeight;  // the canvas default
   const near2 = 0.1;
   const far2 = 5;
   const camera2 = new THREE.PerspectiveCamera(fov2, aspect2, near2, far2);
-  camera2.position.z = 2;
+  camera2.position.z = 1;
 
   const squareScene = new THREE.Scene();
   const circleScene = new THREE.Scene();
@@ -44,7 +44,7 @@ import * as THREE from 'three';
     cubeMesh.rotation.x = time;
     cubeMesh.rotation.y = time;
 
-    circleMesh.rotation.x = time;
+    // circleMesh.rotation.x = time;
     circleMesh.rotation.y = time;
    
     renderer.render(squareScene, camera);
