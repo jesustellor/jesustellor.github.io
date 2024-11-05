9d8ed108287c9b6308cdb4e83e73e17b0036bbbf
Jesus T. <jesustellor@gmail.com>
2024-11-05 05:46:53 +0000

    adding point light helper and grid helper, also using orbit controls, just messing around to learn

--- pages/matrix.js
+++ pages/matrix.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 
+import {OrbitControls} from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
+
 const scene = new THREE.Scene();
 const canvas = document.querySelector('#matrix');
 
@@ -16,15 +18,28 @@ const renderer = new THREE.WebGLRenderer({canvas});
 camera.position.setZ(30);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+const material = new THREE.MeshStandardMaterial({color: 0x00ff00});
 const circle = new THREE.Mesh(geometry, material);
 scene.add(circle);
+
+const light = new THREE.PointLight(0xffffff);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+light.position.set(8,8,8);
+scene.add(light, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(light);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
     requestAnimationFrame(animate);
 
     circle.rotation.x += 0.01;
     circle.rotation.y += 0.01;
+
+    controls.update();
 
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
